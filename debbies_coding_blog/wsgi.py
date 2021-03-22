4194f5970bfed578ffa029cb99c76dc57d5dbee5
Deborah Thompson <deb246@btinternet.com>
2021-03-22 22:06:13 +0000

    first commit

---
+++ debbies_coding_blog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for debbies_coding_blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'debbies_coding_blog.settings')
+
+application = get_wsgi_application()
