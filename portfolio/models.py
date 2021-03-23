fda92450fd72994afcf832bf712d07a80f018007
Deborah Thompson <deb246@btinternet.com>
2021-03-23 15:15:46 +0000

    added model for portfolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#create database table for the projects class
+#each item in the class is a row in the Projects table
+#used migrate then makemigrations in the terminal to add to django project
+#installed pillow as working with images in the table
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='portfolio/images')
+    url = models.URLField(blank=True)
