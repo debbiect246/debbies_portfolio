0c776f6f31c7f8b961bbad8016de4a0ed1a3d66a
Deborah Thompson <deb246@btinternet.com>
2021-04-02 20:45:22 +0100

    added view for blog page

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date') [:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
